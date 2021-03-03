29f27402aff00c01c3ef40e34e986228652b8dc8
bngams <n.boris@devidia.net>
2021-03-03 17:03:18 +0100

    implementation with baackend and frontend

--- backend/http.js
+++ backend/http.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const todos = require('./todo-data');
 const app = express();
 const port = 8282;
+
+app.use(cors()); // allow cors globally
 
 app.get('/todos', (req, res) => {
     res.send(todos);
